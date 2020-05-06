6b4eba4bec30f512ea5bc3d96dc073719c8bbfc2
tanita plaengprawat <tanitaplaeng@gmail.com>
2020-05-06 10:13:18 -0400

    first commit

---
+++ style.js
@@ -0,0 +1,22 @@
+let actualTemp = 58;
+let desiredTemp = 76; 
+
+if (actualTemp < desiredTemp) { 
+    console.log('Run heat')
+} else if (desiredTemp > actualTemp) { 
+    console.log('Run A/C')
+} else { 
+    console.log('Stand by')
+}
+
+//let name1 = Daisy; 
+//let name2 = Forrest; 
+//let name3 = Wolf;
+
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+}
+
+for (let i = 10; i > 0; i--) { 
+    console.log(i);
+}
