7a03d0e7254d434d40616c23865e8b2813df4095
tanita plaengprawat <tanitaplaeng@gmail.com>
2020-05-09 15:13:25 -0400

    updated conditions exericse 2

--- script.js
+++ script.js
@@ -19,17 +19,35 @@ let name3 = 'Jack';
 
 if (name1.length < name2.length && name2.length > name3.length) {
     console.log(name2 + ' is the longest name')
-}   else if (name3.length < name1.length && name1.length < name2.length) {
-    console.log(name2 + ' is the longest name') 
-}   else if (name1.length < name2.length || name1.length < name2.length) { 
-    console.log(name2 + ' is the longest name') 
-}   else if (name2.length < name1.length || name2.length > name3.length) {
-    console.log(name2 + ' is the longest name')
-}   else if (name3.length > name2.length || name3.length < name2.length) {
-    console.log(name2 + ' is the longest name')
-} else { 
-    console.log(name2 + ' is the longest name')
+}   else if (name3.length < name1.length && name2.length < name1.length) {
+    console.log(name1 + ' is the longest name') 
+}   else if (name1.length < name2.length && name2.length < name3.length) { 
+    console.log(name3 + ' is the longest name') 
+}   else if (name1.length === name2.length && name2.length > name3.length) {
+    console.log(`${name1} and ${name2} are tied for the longest name`)
+}   else if (name2.length === name3.length && name3.length > name1.length) {
+    console.log(`${name2} ${name3} are tied for the longest name`)
+}   else if (name1.length === name3.length && name1.length > name2.length) {
+    console.log(`${name1} and ${name3} are tie for the longest name`)
+}   else { 
+    console.log(`${name1}, ${name2}, and ${name3} are all the same length`)
 }
+
+// const name = ['Tom', 'Claire', 'David', 'JJ', 'June']
+
+// function findLongestName(array) {
+//     let longestName = array[0];
+//     for (let name of array) { 
+//         if (name.length > longestName.length) {
+//             longestName = name;
+//         } else {
+//             longestName;
+//         }
+//     }
+//     return longestName;
+// }
+
+console.log(findLongestName(name))
 
 // Loops Exercise 1
 // for Loop
@@ -53,7 +71,7 @@ while (x < 11) {
 
 let a = 10; 
 
-while (a> 0) { 
+while (a > 0) { 
     console.log(a);
     a--;
 }
@@ -77,4 +95,11 @@ do {
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (const number of numbers) {
     console.log(number); 
+}
+
+// Extended Loop Exercise
+
+const initialStringValue = 'four';
+while(initialStringValue.length < 10) {
+    initialStringValue = ' ' + initialStringValue;
 }
